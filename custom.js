73b685093bdec1e6646e76bc003493f3c88b2750
Jayson <xantaren@gmail.com>
2021-09-21 20:14:41 +0800

    Set up portfolio website

---
+++ custom.js
@@ -0,0 +1,55 @@
+// Navbar Sticky Start
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunction()
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Navbar Sticky End
+
+// Smooth Scrolling Start
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 100, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+// Smooth Scrolling End
+
+// Prevent Enter key from submitting form
+$('textarea').keypress(function(e){
+    e.stopPropagation();
+});
